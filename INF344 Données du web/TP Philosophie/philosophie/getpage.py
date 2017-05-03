7fc2db7f02122779af0596ab7215171b9a37238b
Mohamed Al ani <Mohamed Al ani>
2017-05-03 23:46:36 +0200

    maj tp donnees web

--- INF344 Données du web/TP Philosophie/philosophie/getpage.py
+++ INF344 Données du web/TP Philosophie/philosophie/getpage.py
@@ -7,18 +7,21 @@ from bs4 import BeautifulSoup
 from json import loads
 from urllib.request import urlopen
 from urllib.parse import urlencode
-
+from pprint import pprint
+from urllib.parse import unquote
+from urllib.parse import urldefrag
 
 # Si vous écrivez des fonctions en plus, faites-le ici
 
 
 def getJSON(page):
     params = urlencode({
-      'key1': 'value1',  # TODO: compléter ceci
-      'key2': 'value2',  # TODO: compléter ceci
-      'key3': 'value3',  # TODO: compléter ceci
+      'format': 'json',  # TODO: compléter ceci
+      'action': 'parse',  # TODO: compléter ceci
+      'prop': 'text',  # TODO: compléter ceci
+      'redirects' : "true",
       'page': page})
-    API = "http://example.com/apiurl"  # TODO: changer ceci
+    API = "https://fr.wikipedia.org/w/api.php"  # TODO: changer ceci
     response = urlopen(API + "?" + params)
     return response.read().decode('utf-8')
 
@@ -26,8 +29,8 @@ def getJSON(page):
 def getRawPage(page):
     parsed = loads(getJSON(page))
     try:
-        title = "FIXME"  # TODO: remplacer ceci
-        content = "FIXME"  # TODO: remplacer ceci
+        title = parsed["parse"]["title"]  # TODO: remplacer ceci
+        content = parsed["parse"]["text"]["*"]  # TODO: remplacer ceci
         return title, content
     except KeyError:
         # La page demandée n'existe pas
@@ -35,15 +38,36 @@ def getRawPage(page):
 
 
 def getPage(page):
-    pass  # TODO: écrire ceci
+    page = page.replace(" ", "_")
+    title, json = getRawPage(page)
+    soup = BeautifulSoup(json, 'html.parser')
+    liste_p = soup.find_all("p", recursive=False)
+    liste_a=[]
+    
+    for item in liste_p:
+        item.find_all("a", href=True)
+        liste_a += [elem for elem in item.find_all("a", href=True)]
+
+    new_list = []
+    
+    for item in liste_a:
+        try: 
+            if item["href"].split("/")[1]=="wiki":
+                elemt = unquote(urldefrag(item["href"].split("/")[2])[0]).replace("_", " ")
+                if elemt not in new_list:
+                    if "Aide:" not in elemt:
+                        new_list.append(elemt)
+        except:
+            continue
+    return title, new_list[:10]   # TODO: écrire ceci
 
 
 if __name__ == '__main__':
     # Ce code est exécuté lorsque l'on exécute le fichier
+
     print("Ça fonctionne !")
-    
+
     # Voici des idées pour tester vos fonctions :
-    # print(getJSON("Utilisateur:A3nm/INF344"))
+    print(getPage("Histoire"))
     # print(getRawPage("Utilisateur:A3nm/INF344"))
     # print(getRawPage("Histoire"))
-
--- INF344 Données du web/TP Philosophie/philosophie/philosophie.py
+++ INF344 Données du web/TP Philosophie/philosophie/philosophie.py
@@ -10,12 +10,50 @@ app = Flask(__name__)
 
 app.secret_key = "TODO: mettre une valeur secrète ici"
 
+global cache
+cache = {}
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', message="Bonjour, monde !")
+    return render_template('index.html')
 
+
+@app.route('/game', methods=['GET'])
+def game(title=None, items=None, score=None):
+    if session["article"] in cache.keys():
+        title1, items1 = cache[session["article"]]
+        session["test_cache"] = 1
+    else :
+        session["test_cache"] = 0
+        title1, items1 = getPage(session["article"])
+        cache[session["article"]] = title1, items1
+    return render_template('game.html', title=title1, items=items1, score=session["score"])
+
+
+@app.route('/new-game', methods=['POST'])
+def new_game():
+    session["article"] = request.form["nom_page"]
+    session["score"] = 0
+    return redirect('/game')
+
+
+@app.route('/move', methods=['POST'])
+def next_page():
+    #if request.form["score"] != session["score"]:
+    #    flash('Mouvement annulé')
+    #    return redirect('/')
+    
+    new_page = request.form["new_page"]
+    
+    if new_page == "Philosophie":
+        flash('Bravo vous avez gagné')
+        return redirect('/')
+    else :
+        session["article"] = request.form["new_page"]
+        session["score"] += 1
+        return redirect('/game')
 # Si vous définissez de nouvelles routes, faites-le ici
+
 
 if __name__ == '__main__':
     app.run(debug=True)
